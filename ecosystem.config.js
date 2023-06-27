f925fadfb3e75889029828b8681561a82f80040c
Mohammed Sami <mohmaedsamy112@gmail.com>
2023-06-27 09:57:43 +0300

    PM2 Config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'myapp',
-      port: '443',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
       script: './.output/server/index.mjs',
     },
   ],
