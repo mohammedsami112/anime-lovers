a02102ddc546562c55acf0044d19dc7941c9fada
Mohammed Sami <mohmaedsamy112@gmail.com>
2023-07-07 12:41:18 +0300

    Add Episode Slug

--- components/home/episodes.vue
+++ components/home/episodes.vue
@@ -9,7 +9,7 @@
       </div>
       <v-row>
         <v-col class="mt-3" cols="12" lg="3" md="4" sm="6" v-for="episode in homeStore.episodes" :key="episode.id">
-          <NuxtLink :to="'/anime/' + episode.anime.id + '-' + episode.anime.slug + '/' + episode.id + '-' + episode.title">
+          <NuxtLink :to="'/anime/' + episode.anime.id + '-' + episode.anime.slug + '/' + episode.id + '-' + episode.slug">
             <v-card>
               <v-img :src="config.public.backendURL + '/storage/episodes/thumbnails/' + episode.thumbnail" cover height="200px"></v-img>
               <v-card-title class="text-center">{{ episode.title }}</v-card-title>
