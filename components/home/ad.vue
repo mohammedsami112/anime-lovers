8e4ec3bab797dc3eb14119622e86a624883544cc
Mohammed Sami <mohmaedsamy112@gmail.com>
2023-07-27 02:03:53 +0300

    ad

--- components/home/ad.vue
+++ components/home/ad.vue
@@ -1,10 +1,25 @@
 <template>
   <v-col class="mt-3 ad" cols="12" lg="4" md="4" sm="6">
-    <img src="../../assets/images/test.jpg" alt="" />
+    <!-- <img src="../../assets/images/test.jpg" alt="" /> -->
+
+    <client-only>
+      <!-- <script src="" data-zone="1412" async data-cfasync="false"></script> -->
+    </client-only>
   </v-col>
 </template>
 
-<script setup></script>
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://alwingulla.com/88/tag.min.js',
+      'data-zone': 1412,
+      async: true,
+      ' data-cfasync': false,
+    },
+  ],
+});
+</script>
 
 <style lang="scss" scoped>
 .ad {
