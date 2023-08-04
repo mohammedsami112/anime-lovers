db4e75b15005c9e41680dcb061a3ff98f35a63ab
Mohammed Sami <mohmaedsamy112@gmail.com>
2023-08-05 02:44:35 +0300

    set proxy

---
+++ config/goutte.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+     |-----------------------------------------------------------------------
+     | Symfony Http Client Configuration
+     |-----------------------------------------------------------------------
+     |
+     | A collection of default request options to apply to each request
+     | dispatched by the Symfony Http Client.
+     |
+     | @see {@link https://symfony.com/doc/current/http_client.html}
+     */
+
+    'client' => [
+        'max_redirects' => 0,
+        'proxy' => 'http://scraperapi:d746d3ceb1d8046035973a4d78834eed@proxy-server.scraperapi.com:8001'
+    ],
+];
